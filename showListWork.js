7ac3990c006c366b6498c4e3cd7f6cff761739c5
vladimirvikulin <v.vikulin.im11@kpi.ua>
2023-03-07 22:14:17 +0200

    added a demonstration of using all class methods This commit adds a file that prints to the console the work of methods of the first implementation of the list

---
+++ showListWork.js
@@ -0,0 +1,61 @@
+'use strict'
+
+const { singlyLinkedList } = require('./list.js')
+
+const list = new singlyLinkedList();
+console.log(list);
+console.log('length():', list.length() + '\n');
+
+console.log(list);
+list.append(5)
+console.log('append(5)');
+console.log(list, '\n');
+
+console.log(list);
+list.insert('sad', 1);
+console.log('insert(\'sad\', 1)');
+console.log(list, '\n');
+
+console.log(list);
+list.delete(1);
+console.log('delete(1)');
+console.log(list, '\n');
+
+list.append('sad');
+console.log(list);
+list.deleteAll('sad')
+console.log('deleteAll(\'sad\')');
+console.log(list, '\n');
+
+console.log(list);
+console.log('get(0):', list.get(0) + '\n');
+
+list.append('sad');
+console.log(list);
+const cloneList = list.clone();
+console.log('clone():', cloneList.array + '\n');
+
+list.append(345);
+list.append(true);
+console.log(list);
+list.reverse();
+console.log('reverse():', list.array  + '\n');
+
+list.append(true);
+console.log(list);
+console.log('findFirst(true):', list.findFirst(true) + '\n');
+
+console.log(list);
+console.log('findLast(true):', list.findLast(true) + '\n');
+
+console.log(list);
+list.clear()
+console.log('clear()');
+console.log(list, '\n');
+
+const list2 = new singlyLinkedList();
+list.append(1);
+list2.append(2);
+console.log(list);
+console.log(list2);
+console.log('extend():', list.extend(list2));
